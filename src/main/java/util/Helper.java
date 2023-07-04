2e94991ad4d056b49da7310601e270a2731f1c77
Sofia Di Nonno <sofia@thelucky.app>
2023-07-04 11:09:03 -0300

    New util method to generate random text

---
+++ src/main/java/util/Helper.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.Random;
+
+public class Helper {
+
+    public static String generateRandomText(int length) {
+        char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
