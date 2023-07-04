c8362b8c441154e4b4887a42bd31a8744e465fef
Sofia Di Nonno <sofia@thelucky.app>
2023-07-04 10:55:56 -0300

    New methods and elements for Progress Bar Page

--- src/main/java/ui/pages/ProgressBarPage.java
+++ src/main/java/ui/pages/ProgressBarPage.java
@@ -1,11 +1,40 @@
 package ui.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ProgressBarPage extends BasePage {
 
+    @FindBy(id = "startButton")
+    private WebElement startButton;
+
+    @FindBy(id = "stopButton")
+    private WebElement stopButton;
+
+    @FindBy(id = "progressBar")
+    private WebElement progressBar;
+
+    @FindBy(id = "result")
+    private WebElement resultMessage;
+
     public ProgressBarPage(WebDriver driver) {
         super(driver);
+    }
+
+    public void start(){
+        clickElement(startButton);
+    }
+
+    public void waitForPercentage(String value){
+        waitForAttribute(progressBar, "aria-valuenow", value);
+    }
+
+    public void stop(){
+        clickElement(stopButton);
+    }
+
+    public String getResult(){
+        return resultMessage.getText().substring(0,10);
     }
 }
