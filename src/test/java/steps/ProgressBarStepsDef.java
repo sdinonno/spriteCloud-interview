398eabcb12273ad1f95c6ff6990f69cc6f02112c
Sofia Di Nonno <sofia@thelucky.app>
2023-07-04 10:56:27 -0300

    Complete steps for Progress Bar

--- src/test/java/steps/ProgressBarStepsDef.java
+++ src/test/java/steps/ProgressBarStepsDef.java
@@ -4,27 +4,37 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
+import ui.cucumber.TestContext;
+import ui.pages.ProgressBarPage;
 
 public class ProgressBarStepsDef {
 
-    public ProgressBarStepsDef(){
+    TestContext testContext;
+    ProgressBarPage progressBarPage;
 
+    public ProgressBarStepsDef(TestContext context){
+        testContext = context;
+        progressBarPage = context.getPageObjectManager().getProgressBarPage();
     }
     @Given("the user clicks on Start")
     public void theUserClicksOnStart() {
+        progressBarPage.start();
     }
 
     @And("the user waits for {string} percentage")
     public void theUserWaitsFor(String percentage) {
-
+        progressBarPage.waitForPercentage(percentage);
     }
 
     @When("the user clicks on Stop")
     public void theUserClicksOnStop() {
-
+        progressBarPage.stop();
     }
 
     @Then("the user sees Result is equal to {string}")
-    public void theUserSeesResultIsEqualTo(String arg0) {
+    public void theUserSeesResultIsEqualTo(String number) {
+        String expectedMsg = "Result: ".concat(number);
+        Assert.assertEquals(expectedMsg, progressBarPage.getResult());
     }
 }
